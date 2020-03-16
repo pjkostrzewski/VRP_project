4febdc1f060b5f4c95a3a2ba9caa6cd8211f6b01
Patryk Kostrzewski <pjkostrzewski@gmail.com>
2020-03-16 22:46:52 +0100

    generation bug fixed

--- RoutesContainer.py
+++ RoutesContainer.py
@@ -1,3 +1,4 @@
+from random import sample
 from Route import Route
 import helpers
 
@@ -18,3 +19,25 @@ class RoutesContainer(object):
             distance += route.calculate_distance()
         return distance
     
+    
+def generate_random_routes_container(points):
+    number_of_nodes = int(round(len(points)/helpers.salesmen))
+    results = []
+    
+    def clear_sample():
+        for point in route:
+            points.remove(point)
+            
+    while True:
+        try:
+            route = sample(points, number_of_nodes)
+            clear_sample()
+            if len(points) == 0:
+                break
+        except ValueError:
+            route = points
+            break
+        finally:
+            results.append(Route(route))
+    assert len(results) == helpers.salesmen, "size of results is wrong"
+    return RoutesContainer(results)
--- mtsp_main.py
+++ mtsp_main.py
@@ -1,40 +1,18 @@
-from random import sample
 from BenchmarksUtils import Benchmark
 from Plotter import MatplotlibPlotter
 from Population import Population, FirstPopulation, ChildsPopulation
 from GA import GeneticAlgorithm
 from Route import Route
-from RoutesContainer import RoutesContainer
+from RoutesContainer import generate_random_routes_container
 
-
-def generate_random_routes_container(salesmen, points):
-    number_of_nodes = int(round(len(points)/salesmen))
-    results = []
-    
-    def clear_sample():
-        for point in route:
-            points.remove(point)
-            
-    while True:
-        try:
-            route = sample(points, number_of_nodes)
-            clear_sample()
-        except ValueError:
-            route = points
-            break
-        finally:
-            results.append(Route(route))
-            
-    return RoutesContainer(results)
 
 benchmark = Benchmark(  vrp_path="eil51/moje8_m2.tsp", 
                         solution_path="eil51/moje8_m2-tours.txt")
 
 plotter = MatplotlibPlotter()
 points = benchmark.get_points()
-population = FirstPopulation(points)
-print(len(points[1:]))
-routes_container = generate_random_routes_container(4, points[1:])
+# population = FirstPopulation(points)
+routes_container = generate_random_routes_container(points[1:])
 # routes_container = RoutesContainer([route1, route2])
 # print(routes_container.calculate_distance())
 plotter.draw(points, routes_container)
